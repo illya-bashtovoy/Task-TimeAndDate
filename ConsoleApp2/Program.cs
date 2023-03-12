e42847da458f525640bbde9f6c1741ec25c73f9e
illya-bashtovoy <bashtoff@gmail.com>
2023-03-12 13:52:16 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sistem_Programe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Виведення поточного часу та дати
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine($"Час: {currentTime:HH:mm:ss}");
+            Console.WriteLine($"Дата: {currentTime:D}\n");
+
+            // Створення трьох завдань для виведення поточного часу
+            var task1 = Task.Run(() => Console.WriteLine($"{DateTime.Now:HH:mm:ss}"));
+            var task2 = Task.Run(() => Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss}"));
+            var task3 = Task.Run(() => Console.WriteLine($"{DateTimeOffset.Now:HH:mm:ss}"));
+
+            // Очікування завершення всіх завдань
+            Task.WaitAll(task1, task2, task3);
+        }
+    }
+}
